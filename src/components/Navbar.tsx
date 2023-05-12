dd0c8188d050ad99d27195c593d0a64967daaf03
kaleabd <kaleabdereje94@gmail.com>
2023-05-12 11:30:19 +0300

    making the nav bar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Logo from '../assets/et-logo.png'
 import {FiSearch} from 'react-icons/fi'
+import {AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
@@ -9,17 +10,18 @@ const Navbar = () => {
     text-primary w-11/12 mx-auto
     '>
         
-        <div className='flex items-center'>
-            <img src={Logo} alt="" className='w-[25%]'/>
-            <ul className='flex gap-6 mt-14 text-lg '>
+        <div className='flex items-center justify-between'>
+            <img src={Logo} alt="" className='lg:w-[25%] w-[50%]'/>
+            <ul className='lg:flex gap-6 mt-14 text-lg hidden '>
                 <li className='font-railway font-bold'>Home</li>
                 <li>Plan a trip</li>
                 <li>Flight info</li>
                 <li>Service</li>
                 <li>Help</li>
             </ul>
+            <AiOutlineMenu className="text-4xl mt-14 lg:hidden "/>
         </div>
-        <div className='flex items-center mt-14 gap-6'>
+        <div className='lg:flex items-center mt-14 gap-6 hidden'>
             <FiSearch />
             <select name="" id="" className='outline-none bg-transparent'>
                 <option value="EN">EN</option>
